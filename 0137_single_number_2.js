897a0caffc781977e9fbb528d983a784dc20f733
Gennady Basov <gbasov@albelli.net>
2020-06-22 12:05:43 +0200

    137 explanation

--- 0137_single_number_2.js
+++ 0137_single_number_2.js
@@ -7,9 +7,19 @@ const singleNumber = nums => {
     let doubles = 0;
 
     for (num of nums) {
+        // if a new bit was in singles, then remove it from singles
+        // if a new bit was not in singles and not in doubles, then add it to singles
         singles = (~doubles) & (singles ^ num);
+
+        // add to doubles if it's not in singles and in doubles
+        // remove from doubles if it's in singles or in doubles
         doubles = (~singles) & (doubles ^ num);
+
+        console.log(num, 'singles:', singles.toString(2), 'doubles:', doubles.toString(2));
     }
 
     return singles;
 };
+
+console.log(singleNumber([2,2,3,2]));
+// console.log(singleNumber([0,1,0,1,0,1,99]));
