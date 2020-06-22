56623a9c8fa54956d174a416f7b3d2ea92dc3a3d
Gennady Basov <gbasov@albelli.net>
2020-06-22 12:14:01 +0200

    137 comments

--- 0137_single_number_2.js
+++ 0137_single_number_2.js
@@ -7,7 +7,9 @@ const singleNumber = nums => {
     let doubles = 0;
 
     for (num of nums) {
+        // Add to singles if it's not in doubles. Also remove from singles if it's in there
         singles = (~doubles) & (singles ^ num);
+        // Add to doubles if it's not in singles. Also remove from doubles if it's in there
         doubles = (~singles) & (doubles ^ num);
     }
 
