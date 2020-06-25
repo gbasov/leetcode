1bfed83aa4e9108efb9c81abe50f161f25e3cb80
Gennady Basov <gbasov@albelli.net>
2020-06-25 12:23:46 +0200

    287. Find the Duplicate Number

---
+++ 0287_find_the_duplicate_number.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const findDuplicate = nums => {
+    let tortoise = nums[0];
+    let hare     = nums[nums[0]];
+
+    while (hare !== tortoise) {
+        tortoise = nums[tortoise];
+        hare = nums[nums[hare]];
+        
+    }
+
+    tortoise = nums[0];
+    hare = nums[hare];
+
+    while (hare !== tortoise) {
+        tortoise = nums[tortoise];
+        hare = nums[hare];
+    }
+
+    return tortoise;
+};
